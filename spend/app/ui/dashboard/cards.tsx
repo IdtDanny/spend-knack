09cd6da681e5b05ed4d582d909deca9d432bf19c
Idt Danny <idtdanny@gmail.com>
2024-06-29 14:49:40 +0200

    Newly modified - start a fresh

--- spend/app/ui/dashboard/cards.tsx
+++ spend/app/ui/dashboard/cards.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -16,7 +16,7 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+      {/* NOTE: Uncomment this code in Chapter 9 */}
 
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
