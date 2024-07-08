b5f49a63cfab1e23ed1ccf889e8cad674edd0223
Idt Danny - Schwester 1 <idtdanny@gmail.com>
2024-07-08 22:59:13 +0200

    teal theme progress

--- spend/app/ui/acme-logo.tsx
+++ spend/app/ui/acme-logo.tsx
@@ -1,5 +1,6 @@
 // import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { SavingsIcon } from '@/app/ui/dashboard/account-setting-02-stroke-rounded';
 
 export default function AcmeLogo() {
   return (
@@ -7,6 +8,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <SavingsIcon className='text-white mr-5 ml-2' />
       <p className="text-[30px]">SpendKnack</p>
     </div>
   );
