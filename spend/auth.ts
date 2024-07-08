bd9b8e09615bb6c9f007233732de1734ac27c825
Idt Danny <idtdanny@gmail.com>
2024-07-08 12:55:36 +0200

    All about sign in Handled

--- spend/auth.ts
+++ spend/auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
-import { z } from 'zod';
+import { authConfig } from "./auth.config";
 import { sql } from '@vercel/postgres';
+import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
-import { parse } from "path";
+// import { parse } from "path";
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
