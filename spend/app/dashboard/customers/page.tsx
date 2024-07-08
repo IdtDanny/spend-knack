9d9921511e8fae70d125c9c9792908eabdd9c851
Idt Danny <idtdanny@gmail.com>
2024-07-08 13:15:43 +0200

    Metadatas added

--- spend/app/dashboard/customers/page.tsx
+++ spend/app/dashboard/customers/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+
 export default function Page() {
-    return(
+    return (
         <p>Customers Page</p>
     );
 }
