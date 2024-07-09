366a1204e47f1fc7c933f00f621e13ae7d1f70a7
Idt Danny <idtdanny@gmail.com>
2024-07-09 13:00:14 +0200

    Fixing edit-form

--- spend/app/dashboard/invoices/error.tsx
+++ spend/app/dashboard/invoices/error.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function Error({
     error,
@@ -10,6 +10,7 @@ export default function Error({
     reset: () => void;
 }) {
     useEffect(() => {
+        // Optionally log the error to an error reporting service
         console.error(error);
     }, [error]);
 
@@ -17,10 +18,14 @@ export default function Error({
         <main className="flex h-full flex-col items-center justify-center">
             <h2 className="text-center">Something went wrong!</h2>
             <button
-                className="mt-4 rounded-md bg-teal-500 px-4 py-2 text-sm text-white transition-colors hover:bg-teal-400"
+                className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
                 onClick={
+                    // Attempt to recover by trying to re-render the invoices route
                     () => reset()
-                }> Try again</button>
+                }
+            >
+                Try again
+            </button>
         </main>
     );
 }
