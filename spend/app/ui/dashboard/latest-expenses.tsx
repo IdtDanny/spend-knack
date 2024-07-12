a10d7aa063cab31d1ec5439747d542f7b596dc09
Idt Danny <idtdanny@gmail.com>
2024-07-12 16:27:07 +0200

    Key error solved

--- spend/app/ui/dashboard/latest-expenses.tsx
+++ spend/app/ui/dashboard/latest-expenses.tsx
@@ -14,7 +14,6 @@ export default async function LatestExpenses() {
             </h2>
             <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
                 {/* NOTE: Uncomment this code in Chapter 9 */}
-
                 <div className="bg-white px-6">
                     {latestExpenses.map((expense, i) => {
                         return (
--- spend/app/ui/dashboard/nav-links.tsx
+++ spend/app/ui/dashboard/nav-links.tsx
@@ -18,18 +18,8 @@ import { AccountSetting02Icon, AffiliateIcon } from '@/app/ui/dashboard/icon-tem
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   // { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
-  {
-    name: 'Reason',
-    href: '/dashboard/reason',
-    icon: ArchiveBoxIcon,
-    // icon: AffiliateIcon,
-  },
-  {
-    name: 'Expenses',
-    href: '/dashboard/expense',
-    icon: CreditCardIcon,
-    // icon: AffiliateIcon,
-  },
+  { name: 'Reason', href: '/dashboard/reason', icon: ArchiveBoxIcon },
+  { name: 'Expenses', href: '/dashboard/expense', icon: CreditCardIcon },
   // { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
   // { name: 'My Profile', href: '/dashboard/profile', icon: AccountSetting02Icon },
   { name: 'My Profile', href: '/dashboard/profile', icon: UserIcon },
@@ -42,17 +32,19 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                'bg-sky-100 text-teal-600': pathname === link.href,
-              }
-            )}>
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
+          <div>
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                {
+                  'bg-sky-100 text-teal-600': pathname === link.href,
+                }
+              )}>
+              <LinkIcon className="w-6" />
+              <p className="hidden md:block">{link.name}</p>
+            </Link>
+          </div>
         );
       })}
     </>
