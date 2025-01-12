dd10a3951e05ca77cdd0d78373f72a97a6513f5c
Idt Danny <68943213+IdtDanny@users.noreply.github.com>
2025-01-12 12:04:19 +0200

    Update login-form.tsx - Changing Cancel
    Updating login form to pick back rather than Cancel

--- spend/app/ui/login-form.tsx
+++ spend/app/ui/login-form.tsx
@@ -75,7 +75,8 @@ export function LoginForm() {
                     
                     <Link href="/">
                         <Cancel className="mt-4 w-full">
-                            Cancel <HandThumbDownIcon className="ml-2 h-5 w-5 text-gray-50" />
+                            Back 
+{/*                             <HandThumbDownIcon className="ml-2 h-5 w-5 text-gray-50" /> */}
                         </Cancel>
                     </Link>
                     
